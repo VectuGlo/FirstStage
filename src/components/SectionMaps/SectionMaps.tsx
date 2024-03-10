86fa338829220fc6f85e8a499c8be5b09a65baab
VectuPro <lapkin.alex96@gmail.com>
2024-03-10 19:15:10 +1000

    create full map1

--- src/components/SectionMaps/SectionMaps.tsx
+++ src/components/SectionMaps/SectionMaps.tsx
@@ -1,11 +1,20 @@
 import { HiDevicePhoneMobile } from 'react-icons/hi2';
 import { TfiEmail } from 'react-icons/tfi';
 import { TbHomeSearch } from 'react-icons/tb';
+import { FaWhatsapp } from 'react-icons/fa';
 import stylesBootstrap from 'bootstrap/dist/css/bootstrap.module.css';
 import styles from './SectionMaps.module.css';
 import cn from 'classnames';
 import Separator from '../Separator/Separator';
 import YandexMaps from '../YandexMaps/YandexMaps';
+import Button from '../Button/Button';
+
+const icons = [
+  <HiDevicePhoneMobile />,
+  <TfiEmail />,
+  <TbHomeSearch />,
+  <FaWhatsapp />,
+];
 
 function SectionMaps() {
   return (
@@ -22,42 +31,68 @@ function SectionMaps() {
               <YandexMaps />
             </div>
             <div
-              className={cn(stylesBootstrap['col-6'], styles['ofice-content'])}
+              className={cn(
+                stylesBootstrap['col-6'],
+                stylesBootstrap['d-flex'],
+                stylesBootstrap['justify-content-center']
+              )}
             >
-              <h4>Офис во Владивостоке</h4>
-              <div className={stylesBootstrap['']}>
-                <a
-                  data-design-type="text"
-                  data-draggable="true"
-                  data-title="Телефон"
-                  href="tel:+79243358311"
-                >
-                  <HiDevicePhoneMobile />
-                  +7 (924) 335-83-11
-                </a>
+              <div className={styles['ofice-content']}>
+                <h4>Офис во Владивостоке</h4>
+                <ul className={styles['ofice-content-ul']}>
+                  <li className={styles['ofice-content-li']}>
+                    {icons[0]}
+                    <a
+                      data-design-type="text"
+                      data-draggable="true"
+                      data-title="Телефон"
+                      href="tel:+79243358311"
+                    >
+                      +7 (924) 335-83-11
+                    </a>
 
-                <a
-                  data-design-type="text"
-                  data-draggable="true"
-                  data-title="Email"
-                  href="mailto:office@asiasalesauto.ru"
-                  target="_blank"
-                >
-                  <TfiEmail />
-                  office@asiasalesauto.ru
-                </a>
-              </div>
-              <div
-                className={styles['addressDiv']}
-                data-design-type="text"
-                data-draggable="true"
-                data-title="Адрес"
-              >
-                <TbHomeSearch />
-                <div className={cn(styles['addressDivContent'])}>
-                  <span>г. Владивосток, ул. Камская, д. 5Б</span>
-                  <span>1 этаж, 1 офис</span>
-                </div>
+                    <a
+                      href="https://wa.me/+79243358311?text=Здравствуйте,%20пишу%20с%20asiasalesauto.ru"
+                      data-design-type="button"
+                      data-draggable="true"
+                      data-title="WhatsApp"
+                      target="_blank"
+                    >
+                      <Button
+                        className={cn(
+                          stylesBootstrap['d-flex'],
+                          stylesBootstrap['gap-3']
+                        )}
+                      >
+                        {icons[3]} Написать нам
+                      </Button>
+                    </a>
+                  </li>
+                  <li className={styles['ofice-content-li']}>
+                    {icons[1]}
+                    <a
+                      data-design-type="text"
+                      data-draggable="true"
+                      data-title="Email"
+                      href="mailto:office@asiasalesauto.ru"
+                      target="_blank"
+                    >
+                      office@asiasalesauto.ru
+                    </a>
+                  </li>
+                  <li className={styles['ofice-content-li']}>
+                    {icons[2]}
+                    <div
+                      className={styles['addressDiv']}
+                      data-design-type="text"
+                      data-draggable="true"
+                      data-title="Адрес"
+                    >
+                      <span>г. Владивосток, ул. Камская, д. 5Б,</span>
+                      <span>1 этаж, 1 офис</span>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
