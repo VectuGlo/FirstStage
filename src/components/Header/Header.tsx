e710ec18ffe54093ca51a65cc747af215ebe0c60
VectuPro <lapkin.alex96@gmail.com>
2024-03-17 13:09:40 +1000

    add new icons messages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,31 @@
+// import { FaWhatsapp } from 'react-icons/fa';
+import { LiaTelegramPlane } from 'react-icons/lia';
+import { IoLogoInstagram } from 'react-icons/io5';
 import stylesBootstrap from 'bootstrap/dist/css/bootstrap.min.module.css';
 import styles from './Header.module.css';
 import cn from 'classnames';
+import Button from '../Button/Button';
+
+const iconsMessages = [
+  {
+    id: 'iconMess01',
+    href: 'https://t.me/AsiaSalesAuto',
+    icon: <LiaTelegramPlane />,
+    messager: 'Telegram',
+  },
+  // {
+  //   id: 'iconMess02',
+  //   href: 'https://t.me/AsiaSalesAuto',
+  //   icon: <FaWhatsapp />,
+  //   messager: 'Telegram',
+  // },
+  {
+    id: 'iconMess03',
+    href: 'https://www.instagram.com/asiasalesauto',
+    icon: <IoLogoInstagram />,
+    messager: 'Instagram',
+  },
+];
 
 const links = [
   { id: 'navLink01', textLink: 'Этапы работы' },
@@ -18,12 +43,13 @@ function Header() {
           <div
             className={cn(
               stylesBootstrap['row'],
+              stylesBootstrap['d-flex'],
               stylesBootstrap['align-items-center'],
               stylesBootstrap['justify-content-between']
             )}
           >
             <div
-              className={cn(stylesBootstrap['col-md-2'], styles['logo-div'])}
+              className={cn(stylesBootstrap['col-md-4'], styles['logo-div'])}
             >
               <img
                 className={styles['logo-img']}
@@ -32,10 +58,29 @@ function Header() {
                 data-draggable="true"
                 src="/logo.webp"
               />
+              <div
+                className={cn(stylesBootstrap['col-md-1'], styles['tlg-div'])}
+              >
+                {iconsMessages.map((item) => {
+                  return (
+                    <a
+                      key={item.id}
+                      href={item.href}
+                      data-draggable="true"
+                      data-title={item.messager}
+                      target="_blank"
+                    >
+                      <Button className={styles['header-btn']}>
+                        {item.icon}
+                      </Button>
+                    </a>
+                  );
+                })}
+              </div>
             </div>
 
             <div
-              className={cn(stylesBootstrap['col-md-9'], styles['header-nav'])}
+              className={cn(stylesBootstrap['col-md-6'], styles['header-nav'])}
             >
               <ul
                 className={cn(
